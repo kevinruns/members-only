128dff9febb0911fb521a3a0c950fb85d17a3657
Kevin Lenehan <klenehan123@gmail.com>
2021-04-29 21:14:22 +0100

    basic working with show.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,18 +2,35 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!
 
-  def new 
+  def new
     @post = Post.new
   end
 
   def create
-    @post = Post.new(post_params)
+#    @post = Post.new(post_params)
+
+    @post = current_user.post.build(post_params)
+    @post.user_id = current_user.id
 
     if @post.save
       redirect_to @post
     else
       render :new
     end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root to: "posts#index"
 
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index, :show]
 
 end
