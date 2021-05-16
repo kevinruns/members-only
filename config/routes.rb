563b00fb49a4fd3b1fad9051a4fa9a14fafa0ef2
Kevin Lenehan <klenehan123@gmail.com>
2021-05-16 14:11:52 +0100

    problem with edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "posts#index"
-  
   resources :posts, only: [:new, :create, :index, :show]
-  resources :users, only: [:index, :destroy, :show]
+#  resources :users, only: [:index, :destroy, :show]
 
+  root to: "posts#index"
 
   # scope "admin" do
   #   get "users", to: "users#index"
